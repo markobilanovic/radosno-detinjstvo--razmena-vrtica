ae7f5bb9fa20f3897fb5312044746a622ee60815
markobilanovic <bilanovic90@gmail.com>
2023-04-07 00:39:00 +0200

    fix links #2

--- pages/rezultati/index.js
+++ pages/rezultati/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import styles from '@/styles/Home.module.css';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 export default function Home() {
   const router = useRouter();
